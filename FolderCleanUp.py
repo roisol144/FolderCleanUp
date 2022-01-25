ff6bb31c7e823c5dd7dd440afbedf8129656bc4c
Roi Solomon <roisol144@gmail.com>
2022-01-25 14:13:46 +0200

    Using suffix lists

--- FolderCleanUp.py
+++ FolderCleanUp.py
@@ -1,37 +1,45 @@
-import os 
+import os
 import shutil
 from watchdog.observers import Observer
 import time
 from watchdog.events import FileSystemEventHandler
 
-# Assign these folders.
-downloads_folder = ""
-pictures_folder = ""
-documents_folder = ""
+if __name__ == "__main__":
+
+    downloads_folder = "/Users/USER/Downloads"
+    pictures_folder = "/Users/USER/Pictures"
+    documents_folder = "/Users/USER/Documents"
+    music_folder = "/Users/USER/Music"
+
+    suffix_audio = [".mp3", ".m4a", ".m4r", ".wma"]
+    suffix_image = [".jpg", ".JPEG", ".jpeg", ".png", ".gif"]
+    suffix_docs = [".doc", ".docx", ".pdf", ".xml", ".xla", ".xlsx"]
 
 
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        for file in os.listdir(downloads_folder):
-            if file.endswith(".jpeg") or file.endswith(".png") or file.endswith(".jpg"):
-                shutil.move((os.path.join(downloads_folder, file)), (os.path.join(pictures_folder, file)))
-            elif file.endswith(".pdf"): 
-                shutil.move((os.path.join(downloads_folder, file)), (os.path.join(documents_folder, file)))
+    class MyHandler(FileSystemEventHandler):
+        def on_modified(self, event):
+            for file in os.listdir(downloads_folder):
+                filename, file_extension = os.path.splitext(file)
 
-go_recursively = True
-observer = Observer()
-event_handler = MyHandler()
-observer.schedule(event_handler, downloads_folder, recursive=go_recursively)
-observer.start()
+                if file_extension in suffix_image:
+                    shutil.move((os.path.join(downloads_folder, file)), (os.path.join(pictures_folder, file)))
+                if file_extension in suffix_docs:
+                    shutil.move((os.path.join(downloads_folder, file)), (os.path.join(documents_folder, file)))
+                elif file_extension in suffix_audio:
+                    shutil.move((os.path.join(downloads_folder, file)), (os.path.join(music_folder, file)))
 
-try:
-    while True:
+
+    observer = Observer()
+    event_handler = MyHandler()
+    observer.schedule(event_handler, downloads_folder, recursive=True)
+    print("Watchdog Starts... ")
+    observer.start()
+    print("Watchdog Tracking... ")
+
+    try:
         time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
-    
-    
+    except KeyboardInterrupt:
+        observer.stop()
 
 
 
